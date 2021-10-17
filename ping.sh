c0f59e3aa240340b969b9d83bd46da3b368c813a
JohnnyTengu <61974778+JohnnyTengu@users.noreply.github.com>
2021-10-17 09:50:39 +0300

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,1 +1,12 @@
-ping $WHAT_PING -c 4
+#!/bin/bash
+# Redefine myvar to myarray using parenthesis
+myarray=($HOSTS)
+
+echo "My array: ${myarray[@]}"
+echo "Number of elements in the array: ${#myarray[@]}"
+
+for i in "${myarray[@]}"
+do
+  echo "$i"
+  ping $i -c 2
+done
